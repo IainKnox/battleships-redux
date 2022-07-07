647f0e9d6bb8af86019c336c1a92477abbf508a3
Iain Knox <iain.knox1@btinternet.com>
2022-07-07 14:50:35 +0000

    create structure for game board and function to print it out

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Battleship game written in python. First iteration will be
+basic version, drawing a board and allowing a user to guess
+the location of randomly placed ships. Building on that to
+allow for the user the place their own ships. Then adding
+an computer player to create a 2 player game.
+"""
+
+from random import randint
+# used to create the random co ordinates
+from time import sleep
+# used to create pauses in the game
+
+# start by defining a 10 x 10 game board
+board = []
+
+for num in range(0,10):
+
+    board.append(["O"] * 10)
+    # the "O" represents a space on the board multiplied
+    # by a predetermined board size, in this case 10.
+
+def print_board(board):
+    """
+    create a function that generates the game board
+    """
+
+    for row in board:
+        print(" ".join(row))
+
+print_board(board)
